61a04d48217d80f8f70874c340cd4f6a3eb2f4ba
Shayan Fallahi <shayan.flh.04@gmail.com>
2021-02-11 17:05:55 +0330

    change db

--- Iradiba/settings.py
+++ Iradiba/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,12 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
-
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -72,17 +69,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Iradiba.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'b1wlqw4wdxkv7rn9sxc2',
+        'USER': 'uq3ub4idhoq0k3uhrehq',
+        'PASSWORD': 'JdHtAxlFhdsqN4g5iUYe',
+        'HOST': 'b1wlqw4wdxkv7rn9sxc2-postgresql.services.clever-cloud.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -102,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -116,12 +114,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Iradiba/static')
 ]
